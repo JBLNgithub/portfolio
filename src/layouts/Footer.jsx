bb038e045cd6afa16e8e4c538e5425a3340c94a3
JBLN <jashbalon@gmail.com>
2026-06-22 20:24:08 +0200

    update mail

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <footer>
             <h2>Contact</h2>
 
-            <a href='mailto:jashbalon@gmail.com' target="_blank" className='hover:text-neutral-800 hover:bg-sky-500 px-3 py-2 rounded-2xl'>
+            <a href='mailto:jose.balon.mail@gmail.com' target="_blank" className='hover:text-neutral-800 hover:bg-sky-500 px-3 py-2 rounded-2xl'>
                 <MdOutlineEmail className='inline mb-1 mr-1' />
-                jashbalon@gmail.com
+                jose.balon.mail@gmail.com
             </a>
             <div className='mt-20'>
             <Logo className='inline-block w-30' />
